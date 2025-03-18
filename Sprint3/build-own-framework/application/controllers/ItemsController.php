2b2bae558747ed1c22eda1f4aef802be0e3020e9
Josh-Techie <youse2002@outlook.fr>
2025-03-18 14:22:03 +0000

    -m
    Fix:Model-Class

--- Sprint3/build-own-framework/application/controllers/ItemsController.php
+++ Sprint3/build-own-framework/application/controllers/ItemsController.php
@@ -1,26 +1,29 @@
 <?php
-// application/controllers/ItemsController.php
 
 class ItemsController extends Controller {
 
-    function view($id = null, $name = null) {
-        $this->set('title', $name . ' - My Todo List App');
-        $this->set('todo', $this->Item->select($id));
-    }
+	function view($id = null,$name = null) {
+	
+		$this->set('title',$name.' - My Todo List App');
+		$this->set('todo',$this->_model->select($id));
 
-    function viewall() {
-        $this->set('title', 'All Items - My Todo List App');
-        $this->set('todo', $this->Item->selectAll());
-    }
+	}
+	
+	function viewall() {
 
-    function add() {
-        $todo = $_POST['todo'];
-        $this->set('title', 'Success - My Todo List App');
-        $this->set('todo', $this->Item->query('insert into items (item_name) values (\'' . mysql_real_escape_string($todo) . '\')'));
-    }
+		$this->set('title','All Items - My Todo List App');
+		$this->set('todo',$this->_model->selectAll());
+	}
+	
+	function add() {
+		$todo = $_POST['todo'];
+		$this->set('title','Success - My Todo List App');
+		$this->set('todo',$this->_model->query('insert into items (item_name) values (\''.mysql_real_escape_string($todo).'\')'));	
+	}
+	
+	function delete($id = null) {
+		$this->set('title','Success - My Todo List App');
+		$this->set('todo',$this->_model->query('delete from items where id = \''.mysql_real_escape_string($id).'\''));	
+	}
 
-    function delete($id = null) {
-        $this->set('title', 'Success - My Todo List App');
-        $this->set('todo', $this->Item->query('delete from items where id = \'' . mysql_real_escape_string($id) . '\''));
-    }
 }
--- Sprint3/build-own-framework/library/controller.class.php
+++ Sprint3/build-own-framework/library/controller.class.php
@@ -1,15 +1,16 @@
 <?php
 class Controller {
-	
+
 	protected $_model;
 	protected $_controller;
 	protected $_action;
 	protected $_template;
 
 	function __construct($model, $controller, $action) {
-		
+
 		$this->_controller = $controller;
 		$this->_action = $action;
+
 		$this->_model = new $model;
 		$this->_template = new Template($controller,$action);
 
@@ -21,5 +22,5 @@ class Controller {
 
 	function __destruct() {
 			$this->_template->render();
-	}	
+	}
 }
