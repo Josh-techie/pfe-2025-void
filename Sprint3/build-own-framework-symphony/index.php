757119e1500b2bea5f7ae71129f609ef56c0545c
Josh-Techie <youse2002@outlook.fr>
2025-03-20 10:39:21 +0000

    -m
    Feature:Refactor-Code-Framework

--- Sprint3/build-own-framework-symphony/index.php
+++ Sprint3/build-own-framework-symphony/index.php
@@ -1,11 +1,9 @@
 <?php
-// build-own-framework-symphony
+// framework/index.php
+require_once __DIR__.'/init.php';
 
-$name = $_GET['name'] ?? 'World!';
+$name = $request->query->get('name', 'World');
 
-// add the header
-
-header('Content-Type: text/html; charset=utf-8');
-printf('Hello %s', htmlentities($name, ENT_QUOTES, 'utf-8'));
-
+$response->setContent(sprintf('Hello %s', htmlspecialchars($name, ENT_QUOTES, 'UTF-8')));
+$response->send();
 ?>
